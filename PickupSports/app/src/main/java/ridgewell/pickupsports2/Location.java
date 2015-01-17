8014bcccb645819c504c736066627ce8a1162b26
Cameron Ridgewell <cameronridgewell@Camerons-MacBook-Pro.local>
2015-01-16 18:23:22 -0600

    initial commit with project files and weak user, event, sport, location, and badge structures

---
+++ PickupSports/app/src/main/java/ridgewell/pickupsports2/Location.java
@@ -0,0 +1,20 @@
+package ridgewell.pickupsports2;
+
+/**
+ * Created by cameronridgewell on 1/16/15.
+ */
+public class Location {
+    private String location;
+
+    public Location(String location_) {
+        location = location_;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location_) {
+        location = location_;
+    }
+}
