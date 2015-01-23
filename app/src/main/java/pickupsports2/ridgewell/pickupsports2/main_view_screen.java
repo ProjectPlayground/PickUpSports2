cabbc99a073a6d47a30f244773a7191cada73734
Cameron Ridgewell <cameron.ridgewell@gmail.com>
2015-01-22 23:24:17 -0600

    made it so that list items may be clicked

--- app/src/main/java/pickupsports2/ridgewell/pickupsports2/main_view_screen.java
+++ app/src/main/java/pickupsports2/ridgewell/pickupsports2/main_view_screen.java
@@ -22,7 +22,7 @@ import ridgewell.pickupsports2.common.*;
 public class Main_View_Screen extends ListActivity {
 
     ArrayList<Event> arraylist;
-    ListView main_event_list;
+    ListView main_event_listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,21 +47,7 @@ public class Main_View_Screen extends ListActivity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main_view_screen, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+    protected void onListItemClick (ListView l, View v, int position, long id) {
+        Toast.makeText(this, "Clicked row " + position, Toast.LENGTH_SHORT).show();
     }
 }
