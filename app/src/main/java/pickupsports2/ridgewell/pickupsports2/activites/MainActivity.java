ec4154ebaa5bdf5e19139bf0b352d178910714d2
Cameron Ridgewell <cameron.ridgewell@gmail.com>
2015-03-12 18:46:01 -0500

    added groundwork for facebook integration using example login

--- app/src/main/java/pickupsports2/ridgewell/pickupsports2/activites/MainActivity.java
+++ app/src/main/java/pickupsports2/ridgewell/pickupsports2/activites/MainActivity.java
@@ -1,160 +1,114 @@
 package pickupsports2.ridgewell.pickupsports2.activites;
 
-import android.app.FragmentManager;
-import android.app.Fragment;
-import android.content.ClipData;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.internal.view.menu.MenuView;
+import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.TextView;
 
-import pickupsports2.ridgewell.pickupsports2.NavigationDrawerFragment;
+import com.facebook.Request;
+import com.facebook.Response;
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.UiLifecycleHelper;
+import com.facebook.model.GraphUser;
+
 import pickupsports2.ridgewell.pickupsports2.R;
-import pickupsports2.ridgewell.pickupsports2.elements.AddEventButton;
 
-public class MainActivity extends ActionBarActivity implements NavigationDrawerFragment.NavigationDrawerCallbacks {
-    final int CREATE_EVENT_CODE = 1;
-
-    /**
-     * FAB for creating new events
-     */
-    AddEventButton create_event;
-
-    /**
-     * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
-     */
-    private NavigationDrawerFragment mNavigationDrawerFragment;
-
-    /**
-     * Used to store the last screen title. For use in {@link #restoreActionBar()}.
-     */
-    private CharSequence mTitle;
-
-    /**
-     * String that contains the value of each options menu button for a given fragment
-     */
-    private String optionButton;
+public class MainActivity extends ActionBarActivity {
+    // Create, automatically open (if applicable), save, and restore the
+    // Active Session in a way that is similar to Android UI lifecycles.
+    private UiLifecycleHelper uiHelper;
+    private View otherView;
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.activity_main_view_screen);
-
-        /* Navigation Drawer */
-        mNavigationDrawerFragment = (NavigationDrawerFragment)
-                getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
-
-        mNavigationDrawerFragment.setUp(
-                R.id.navigation_drawer,
-                (DrawerLayout) findViewById(R.id.drawer_layout));
-
-        //Create Runnable for launching the new activity from create event button
-        Runnable create_launcher = new Runnable() {
-            @Override
-            public void run() {
-                Intent launch_new_event = new Intent(MainActivity.this, CreateEventActivity.class);
-                startActivityForResult(launch_new_event, CREATE_EVENT_CODE);
-            }
-        };
-
-        //Add FAB
-        create_event = new AddEventButton(findViewById(R.id.create_event), create_launcher);
-
-        //Set start screen
-        if (savedInstanceState == null) {
-            onNavigationDrawerItemSelected(1);
-        }
+        setContentView(R.layout.activity_main);
+        // Set View that should be visible after log-in invisible initially
+        otherView = (View) findViewById(R.id.other_views);
+        otherView.setVisibility(View.GONE);
+        // To maintain FB Login session
+        uiHelper = new UiLifecycleHelper(this, callback);
+        uiHelper.onCreate(savedInstanceState);
     }
 
-    /**
-     * Returns the action bar to previous state
-     */
-    public void restoreActionBar() {
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
-        actionBar.setDisplayShowTitleEnabled(true);
-        actionBar.setTitle(mTitle);
+    // Called when session changes
+    private Session.StatusCallback callback = new Session.StatusCallback() {
+        @Override
+        public void call(Session session, SessionState state,
+                         Exception exception) {
+            onSessionStateChange(session, state, exception);
+        }
+    };
+
+    // When session is changed, this method is called from callback method
+    private void onSessionStateChange(Session session, SessionState state,
+                                      Exception exception) {
+        final TextView name = (TextView) findViewById(R.id.name);
+        final TextView gender = (TextView) findViewById(R.id.gender);
+        final TextView location = (TextView) findViewById(R.id.location);
+        // When Session is successfully opened (User logged-in)
+        if (state.isOpened()) {
+            Log.i(TAG, "Logged in...");
+            // make request to the /me API to get Graph user
+            Request.newMeRequest(session, new Request.GraphUserCallback() {
+
+                // callback after Graph API response with user
+                // object
+                @Override
+                public void onCompleted(GraphUser user, Response response) {
+                    if (user != null) {
+                        // Set view visibility to true
+                        otherView.setVisibility(View.VISIBLE);
+                        // Set User name
+                        name.setText("Hello " + user.getName());
+                        // Set Gender
+                        gender.setText("Your Gender: "
+                                + user.getProperty("gender").toString());
+                        location.setText("Your Current Location: "
+                                + user.getLocation().getProperty("name")
+                                .toString());
+                    }
+                }
+            }).executeAsync();
+        } else if (state.isClosed()) {
+            Log.i(TAG, "Logged out...");
+            otherView.setVisibility(View.GONE);
+        }
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        if (!mNavigationDrawerFragment.isDrawerOpen()) {
-            // Only show items in the action bar relevant to this screen
-            // if the drawer is not showing. Otherwise, let the drawer
-            // decide what to show in the action bar.
-            getMenuInflater().inflate(R.menu.navigation_pane, menu);
-            restoreActionBar();
-            menu.findItem(R.id.fragment_action).setTitle(optionButton);
-            return true;
-        }
-        return super.onCreateOptionsMenu(menu);
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        uiHelper.onActivityResult(requestCode, resultCode, data);
+        Log.i(TAG, "OnActivityResult...");
     }
-
-    public void onNavigationDrawerItemSelected(int position) {
-        Fragment fragment = null;
-        switch (position) {
-            case 0:
-                fragment = Profile.newInstance();
-                optionButton = "Edit";
-                //TODO: create edit action
-                break;
-            case 1:
-                optionButton = "Text Here";
-                //Launch to Teams View
-                break;
-            case 2:
-                fragment = new EventFragment();
-                optionButton = "Edit";
-                //TODO: create edit action
-                break;
-            case 3:
-                //Launch to Invitations
-                optionButton = "Text Here";
-                break;
-            case 4:
-                //Launch to Schedule
-                optionButton = "Text Here";
-                break;
-            default:
-        }
-
-        if (fragment != null) {
-            FragmentManager fragmentManager = getFragmentManager();
-            fragmentManager.beginTransaction()
-                    .replace(R.id.container, fragment).commit();
-            mTitle = getResources().getStringArray(R.array.navigation_pane_titles)[position];
-        } else {
-            // error in creating fragment
-            Log.e("MainActivity", "Error in creating fragment");
-        }
-    }
-
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        else if (id == R.id.fragment_action) {
-            //TODO: make edit work?
-            Toast.makeText(this, "Edit", Toast.LENGTH_SHORT).show();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+    public void onResume() {
+        super.onResume();
+        uiHelper.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        uiHelper.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        uiHelper.onDestroy();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        uiHelper.onSaveInstanceState(outState);
     }
 }
-
-
